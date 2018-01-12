cc74e81bdbe72018f32a466ac4ed52dbde560835
Nandira Paturohman <nandirasp@gmail.com>
2018-01-12 08:00:09 +0700

    ui create room

--- src/Components/CreateRoom.js
+++ src/Components/CreateRoom.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 export default class CreateRoom extends Component {
   render () {
     return (
-      <div>
-        <h1>Create Room</h1>
-        <form>
+      <div className='col-md-4 offset-md-4 wrap-content'>
+        <div>
+          <h1>Create Room</h1>
           <div className="form-group">
-            <label>Name of Room</label>
-            <br />
-            <input type="text" className="form" aria-describedby="emailHelp" placeholder="enter room name" />
+            <input type="text" className="v-form" placeholder="Enter Room Name" />
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+          <button type="submit" className="v-button room-button">CREATE</button>
+        </div>
+        <br/>
+        <Link to="/play"><button onClick={() => this.setState({play: false})} className='back-button'></button></Link>
       </div>
     )
   }
