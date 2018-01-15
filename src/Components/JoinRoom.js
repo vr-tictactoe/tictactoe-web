1bb9efa4bebfb30d38e71c14bcd93da118eeceac
Ahmad <matt.syahab@gmail.com>
2018-01-15 18:32:25 +0700

    initial commit

--- src/Components/JoinRoom.js
+++ src/Components/JoinRoom.js
@@ -36,9 +36,11 @@ export default class JoinRoom extends Component {
         window.location.href = `http://localhost:8081/vr/?room=${gameRoom}&player=${this.state.uid}`;
       }else if(room.val().winner !== ''){
         alert('game already finished')
-      }else if(room.val().player2.uid !== '' && room.val().winner === ''){
+      }else if(room.val().player2.uid !== '' && room.val().winner === '' 
+                && room.val().player1.uid !== localStorage.getItem('uid')
+                && room.val().player2.uid !== localStorage.getItem('uid')){
         alert('game already Full')
-      }else if(room.val().player1.uid === localStorage.getItem('uid')){
+      }else if(room.val().player1.uid === localStorage.getItem('uid') || room.val().player2.uid === localStorage.getItem('uid')){
         alert('Welcome Back')
         window.location.href = `http://localhost:8081/vr/?room=${gameRoom}&player=${this.state.uid}`;
       }
