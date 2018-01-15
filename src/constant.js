b13379b154680849b98e299afd3e76841a39ba2f
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2018-01-15 11:38:32 +0700

    Add staging url

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const WEB_URL = 'http://localhost:3000'
-export const VR_URL = 'http://localhost:8081/vr'
+export const WEB_URL = 'https://backup-vr-tictactoe.firebaseapp.com/'
+export const VR_URL = 'https://vrtictactoe.septianfujianto.com/'
